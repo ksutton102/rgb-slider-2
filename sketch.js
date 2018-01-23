4c605351588d1228fa7af826644e75f045c59134
Khalilah Sutton <ksutton@wlapcs.org>
2018-01-23 19:04:55 +0000

    first commit

--- sketch.js
+++ sketch.js
@@ -1,8 +1,25 @@
+var ryeSlider;
+var gyeSlider;
+var byeSlider;
+
 function setup(){
     createCanvas(600,400);
-    background(20,0,0);
+    background(0);
+    
+    ryeSlider = createSlider(0, 255, 100);
+    ryeSlider.position(20,20);
+    
+    gyeSlider = createSlider(0, 255, 100);
+    gyeSlider.position(20,50);
+    
+    byeSlider = createSlider(0, 255, 100);
+    byeSlider.position(20,80);
 }
 
 function draw(){
+    redValue = ryeSlider.value();
+    greenValue = gyeSlider.value();
+    blueValue = byeSlider.value();
     
+    background(redValue, greenValue, blueValue);
 }
